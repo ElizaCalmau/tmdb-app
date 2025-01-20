2927bb23f754974f4a69d6d343b17b4aab3148d4
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-01-20 19:06:54 +0200

    added UI logic for active nav item

--- src/app/_components/Sidebar/SidebarItem.tsx
+++ src/app/_components/Sidebar/SidebarItem.tsx
@@ -2,18 +2,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SidebarNavItem } from "./constants";
+import { usePathname } from "next/navigation";
 
 interface SidebarItemProps {
   navItem: SidebarNavItem; // Use SidebarNavItem type for the navItem prop
 }
 export const SidebarItem: React.FC<SidebarItemProps> = ({ navItem }) => {
   const { title, href, icon_default, icon_active } = navItem;
-
+  const pathname = usePathname();
+  const isActive = pathname === href;
   return (
     <div>
       <Link href={href} className="flex flex-row">
         <Image
-          src={icon_default}
+          src={isActive ? icon_active : icon_default}
           alt="Navigation icon"
           width={24}
           height={24}
