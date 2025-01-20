ace63f9217e23dc824db07b867dbfca31dbc7f18
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-01-20 19:06:00 +0200

    added src for active and default icons to sidebar items array

--- src/app/_components/Sidebar/constants.ts
+++ src/app/_components/Sidebar/constants.ts
@@ -9,25 +9,25 @@ export const SidebarConfig: SidebarNavItem[] = [
   {
     title: "Home",
     href: "/",
-    icon_default: "/icons/home_active.png",
-    icon_active: "/icons/home_defualt.png",
+    icon_active: "/icons/home_active.png",
+    icon_default: "/icons/home.png",
   },
   {
     title: "Watchlist",
     href: "/watchlist",
-    icon_default: "/icons/watchlist_active.png",
-    icon_active: "/icons/watchlist_defualt.png",
+    icon_active: "/icons/watchlist_active.png",
+    icon_default: "/icons/watchlist.png",
   },
   {
     title: "Blog",
     href: "/blog",
-    icon_default: "/icons/blog_active.png",
-    icon_active: "/icons/blog_defualt.png",
+    icon_active: "/icons/blog_active.png",
+    icon_default: "/icons/blog.png",
   },
   {
     title: "Artists",
     href: "/artists",
-    icon_default: "/icons/artists_active.png",
-    icon_active: "/icons/artists_defualt.png",
+    icon_active: "/icons/artists_active.png",
+    icon_default: "/icons/artists.png",
   },
 ];
